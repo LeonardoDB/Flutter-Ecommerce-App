24b317fa80311a98882691201cf996645fe9d3b2
unknown <leodalbosco@hotmail.com>
2020-01-23 18:02:39 -0300

    Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/pages/tabs.page.dart';
+import 'package:shopping/themes/dark-yellow.theme.dart';
+import 'package:shopping/themes/dark.theme.dart';
+import 'package:shopping/themes/light.theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: darkTheme(),
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
+      ),
+    );
+  }
+}
